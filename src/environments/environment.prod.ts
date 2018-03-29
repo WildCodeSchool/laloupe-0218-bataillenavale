1763dd50faec6102b32367898ba4f8a23e720315
AlexandreTeyssier <alextey28@gmail.com>
2018-03-29 13:42:32 +0200

    Fix problem

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDK7BK7sxf6lFImnX_RrTmnDg1Dze2q3aM',
+    authDomain: 'navy-battle.firebaseapp.com',
+    databaseURL: 'https://navy-battle.firebaseio.com',
+    projectId: 'navy-battle',
+    storageBucket: 'navy-battle.appspot.com',
+    messagingSenderId: '275003127901'
+  }
 };
