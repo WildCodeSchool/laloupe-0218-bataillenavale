330436c7586d277b3c7c070686fdf5bd021455b4
Chrys <chrys.wcs@gmail.com>
2018-04-10 15:07:34 +0200

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore } from 'angularfire2/firestore';
 import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs/Observable';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -14,9 +15,10 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  closeResult: string;
   user: Observable<firebase.User>;
   authenticated = false;
-  constructor(public af: AngularFireAuth, private router: Router, ) {
+  constructor(public af: AngularFireAuth, private router: Router, private modalService: NgbModal) {
     this.af.authState.subscribe(
       (auth) => {
         if (auth != null) {
@@ -27,7 +29,7 @@ export class HomeComponent implements OnInit {
     );
   }
   ngOnInit() {
-    
+
   }
   login() {
     this.af.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
@@ -37,6 +39,25 @@ export class HomeComponent implements OnInit {
   logout() {
     this.af.auth.signOut();
     this.authenticated = false;
+  }
+
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
 
