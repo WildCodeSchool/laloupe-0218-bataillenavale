f0d16490a0227061a06aff47d14b50a62fec5ecb
AlexandreTeyssier <alextey28@gmail.com>
2018-04-18 17:09:19 +0200

    Add chatbox into game and fix the chatbox proble m and names

--- src/app/boat-position/boat-position.component.ts
+++ src/app/boat-position/boat-position.component.ts
@@ -175,7 +175,7 @@ export class BoatPositionComponent implements OnInit {
   }
 
   chat() {
-    this.room.chat[this.room.chat.length] = this.me.name + " : " + this.message;
+    this.room.chat[this.room.chat.length] = this.me.name + ' : ' + this.message;
     console.log(this.me.name);
     this.db.doc<Room>('rooms/' + this.roomId).update(this.room);
     this.message = '';
